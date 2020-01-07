78186e61cbe81830438cb77aa4091935e77f6aa9
andrewursh@gmail.com <lose61Axe91lol>
2020-01-08 00:12:12 +0100

    andrei 07.01.2020

--- src/main/java/GoGame/GoClient/Client/Message.java
+++ src/main/java/GoGame/GoClient/Client/Message.java
@@ -9,4 +9,20 @@ public class Message {
         this.header = header.toLowerCase();
         this.value = value;
     }
+
+    public String getHeader(){
+
+        return header;
+    }
+
+    public String getValue(){
+
+        return value;
+    }
+
+    @Override
+    public String toString(){
+
+        return header + '\t' + value;
+    }
 }
