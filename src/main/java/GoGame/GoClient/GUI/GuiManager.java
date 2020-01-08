0bfa1a4c83bc4f712aa851f6ecb86a822f18b331
andrewursh@gmail.com <lose61Axe91lol>
2020-01-08 02:03:56 +0100

    Andreu 07.01

--- src/main/java/GoGame/GoClient/GUI/GuiManager.java
+++ src/main/java/GoGame/GoClient/GUI/GuiManager.java
@@ -1,13 +1,26 @@
 package GoGame.GoClient.GUI;
 
 import GoGame.GoClient.Client.Client;
+import GoGame.GoClient.Client.IMessageReceiver;
+import GoGame.GoClient.Client.Message;
 
-public class GuiManager {
+public class GuiManager implements IMessageReceiver {
 
     private final Client client;
 
     public GuiManager () {
 
         client = new Client ();
+
+    }
+
+    @Override
+    public void receive(Message message) {
+
+    }
+
+    public void connect(String ip, int port){
+        client.setMessageReceiver(this);
+        client.connect(ip, port);
     }
 }
