df34f6aa6f1fa0eff623687909d211fb32f5454d
andrewursh@gmail.com <lose61Axe91lol>
2020-01-08 04:24:45 +0100

    Andrei 08.01

--- src/main/java/GoGame/GoClient/GUI/LoginWindow.java
+++ src/main/java/GoGame/GoClient/GUI/LoginWindow.java
@@ -3,10 +3,26 @@ package GoGame.GoClient.GUI;
 import GoGame.GoClient.Client.Message;
 import GoGame.GoClient.GuiManager;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class LoginWindow extends ReceiverFrame {
+
+    private final TextField hostNameTextField;
+    private final TextField portTextField;
 
     public LoginWindow (GuiManager guiManager) {
 
+        setSize(400,400);
+        setBackground(Color.ORANGE);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setTitle("Go Login");
+
+        setLayout(new FlowLayout());
+        setLayout(null);
     }
 
     @Override
